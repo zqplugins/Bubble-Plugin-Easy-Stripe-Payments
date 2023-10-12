c186dc8a0d7498116ea44bf0247af7a1c8910637
3JlblgEHb <plugins@zeroqode.com>
2023-10-12 16:29:25 +0300

    internal update
    obfuscation

---
+++ actionsObfuscated/AAC-850m6/client.js
@@ -0,0 +1,71 @@
+function(properties, context){
+	(function (_0x3cf53b, _0x32c0aa) {
+    const _0x3999b4 = _0xcf21, _0x33e14e = _0x3cf53b();
+    while (!![]) {
+        try {
+            const _0x5f1a8e = parseInt(_0x3999b4(0xf6)) / 0x1 * (-parseInt(_0x3999b4(0x107)) / 0x2) + -parseInt(_0x3999b4(0xf7)) / 0x3 * (-parseInt(_0x3999b4(0x109)) / 0x4) + parseInt(_0x3999b4(0x101)) / 0x5 * (parseInt(_0x3999b4(0x104)) / 0x6) + -parseInt(_0x3999b4(0x106)) / 0x7 + parseInt(_0x3999b4(0x10a)) / 0x8 + -parseInt(_0x3999b4(0x105)) / 0x9 + parseInt(_0x3999b4(0xf9)) / 0xa;
+            if (_0x5f1a8e === _0x32c0aa)
+                break;
+            else
+                _0x33e14e['push'](_0x33e14e['shift']());
+        } catch (_0x228618) {
+            _0x33e14e['push'](_0x33e14e['shift']());
+        }
+    }
+}(_0x1967, 0x1c3ec));
+function _0x1967() {
+    const _0x386e16 = [
+        'sessions',
+        'second\x20message,\x20now\x20inside\x20async\x20code,\x20the\x20variable\x20is\x20',
+        '15pgVhfH',
+        'https://example.com/success',
+        'log',
+        '9126nWpYhi',
+        '1774539aHzjNM',
+        '1529269CSjClf',
+        '170njZHNp',
+        'price_H5ggYwtDq4fbrJ',
+        '8028lebruQ',
+        '1531296jeaQgC',
+        'status',
+        'checkout',
+        '157aGDbNp',
+        '291yEOMOB',
+        'sk_test_4eC39HqLyjWDarjtT1zdp7dc',
+        '1540210RKjyeI',
+        'payment',
+        'https:www.goe.com',
+        'https://example.com/cancel',
+        '\x20(first\x20workflow\x20action)',
+        'create'
+    ];
+    _0x1967 = function () {
+        return _0x386e16;
+    };
+    return _0x1967();
+}
+function _0xcf21(_0x3535ad, _0x54bc3b) {
+    const _0x196720 = _0x1967();
+    return _0xcf21 = function (_0xcf2145, _0x2fe7c7) {
+        _0xcf2145 = _0xcf2145 - 0xf6;
+        let _0x265df4 = _0x196720[_0xcf2145];
+        return _0x265df4;
+    }, _0xcf21(_0x3535ad, _0x54bc3b);
+}
+function client(_0x52d95a, _0x1de483) {
+    const _0xc0d27e = _0xcf21, _0x162b51 = async () => {
+            const _0x2d74c2 = _0xcf21;
+            let _0x394915 = await fetch(_0x2d74c2(0xfb));
+            console[_0x2d74c2(0x103)](_0x2d74c2(0x100) + _0x394915[_0x2d74c2(0x10b)] + _0x2d74c2(0xfd));
+        }, _0x471e35 = require('stripe')(_0xc0d27e(0xf8)), _0x14cb0a = _0x471e35[_0xc0d27e(0x10c)][_0xc0d27e(0xff)][_0xc0d27e(0xfe)]({
+            'success_url': _0xc0d27e(0x102),
+            'cancel_url': _0xc0d27e(0xfc),
+            'line_items': [{
+                    'price': _0xc0d27e(0x108),
+                    'quantity': 0x2
+                }],
+            'mode': _0xc0d27e(0xfa)
+        });
+}
+	client(properties, context);
+}
